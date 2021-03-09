31d42a279f5bc7c9567f91a28f84011b28ee6de5
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-03-09 11:21:33 +0530

    Login form component task done..Pushed to review

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +15,42 @@ export class LoginComponent implements OnInit {
     private readonly fb: FormBuilder
   ) {
     this.loginForm = this.buildLoginForm();
+    console.log(this.loginForm);
   }
 
   ngOnInit(): void { }
 
   private buildLoginForm(): FormGroup {
     return this.fb.group({
-      email: [null, [Validators.required,]],
-      password: [null, [Validators.required,]]
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required,Validators.pattern('(?=\\D*\\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}')]]
     });
   }
+
+
+  public onLoginClick():void{
+    this.validate();
+    if(this.loginForm.valid){
+      this.login();
+    }
+  }
+
+  private login(){
+    console.log('logged in');
+  }
+
+  get email():FormControl{
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password():FormControl{
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  private validate(){
+    this.email.markAsTouched();
+    this.password.markAsTouched();
+  }
+
 
 }
