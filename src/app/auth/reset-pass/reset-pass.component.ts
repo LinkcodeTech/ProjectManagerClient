bfb3397ff85570251e9320de719e507804d0841f
akshay-0505 <akshaygadhave531@gmail.com>
2021-03-23 21:48:10 +0530

    Reset Password form added

--- src/app/auth/reset-pass/reset-pass.component.ts
+++ src/app/auth/reset-pass/reset-pass.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-pass.component.scss']
 })
 export class ResetPassComponent implements OnInit {
-
-  constructor() { }
+  resetPasswordForm:FormGroup;
+  constructor(
+    private readonly fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.resetPasswordForm=this.buldResetForm();
+  }
+  private buldResetForm():FormGroup{
+    return this.fb.group(
+      {oldPassword:[null,[Validators.required]],
+      newPassword:[null,[Validators.required]],
+      confirmPassword:[null,[Validators.required]]}
+    );
+  }
+  onResetClick(){
+    this.validate();
+    console.log(this.resetPasswordForm);
+  }
+  get oldpassword():FormControl{
+    return this.resetPasswordForm.get('oldPassword') as FormControl;
+  }
+  get newpassword():FormControl{
+    return this.resetPasswordForm.get('newPassword') as FormControl;
+  }
+  get confirmpassword():FormControl{
+    return this.resetPasswordForm.get('confirmPassword') as FormControl;
+  }
+  private validate() {
+    this.oldpassword.markAsTouched();
+    this.newpassword.markAsTouched();
+    this.confirmpassword.markAsTouched();
   }
 
 }
