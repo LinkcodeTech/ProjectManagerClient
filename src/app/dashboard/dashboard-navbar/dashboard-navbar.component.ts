718fc30bb237ac8b0b55f45f0117d1145f5aa979
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-04-16 12:34:42 +0530

    logout alert added in dashboard/navbar

--- src/app/dashboard/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/dashboard/dashboard-navbar/dashboard-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard-navbar',
@@ -21,4 +22,25 @@ export class DashboardNavbarComponent implements OnInit {
     localStorage.clear();
     this.router.navigate(['/']);
   }
+
+  logoutAlert():void{
+    Swal.fire({
+      icon: 'warning',
+      title: 'Are You Sure?',
+      text: 'You will be logged out of the system',
+      showCancelButton: true,
+      confirmButtonText: `logout`,
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    }).then((result) => {
+      if(result.value){
+        this.logout();
+      }
+    })
+   }
+
 }
