b9881047e5e61278562ab592119c7857f741dfb1
akshay-0505 <akshaygadhave531@gmail.com>
2021-04-22 13:01:53 +0530

    edit task form created

--- src/app/dashboard/edit-task/edit-task.component.ts
+++ src/app/dashboard/edit-task/edit-task.component.ts
@@ -1,4 +1,9 @@
+import { User } from './../../interfaces/user.interface';
+import { Project } from './../../interfaces/project.interface';
+import { ProjectDetailsService } from './../../services/project-data-services/project-details.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-task',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-task.component.scss']
 })
 export class EditTaskComponent implements OnInit {
-
-  constructor() { }
+  editTaskForm: FormGroup;
+  developers: any[];
+  isLoading = false;
+  projectId: string;
+  projectName: string;
+  constructor(private readonly fb: FormBuilder,
+    private readonly projectDataService: ProjectDetailsService,
+    private readonly router: Router,
+    private active: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getDeveloperData();
+    this.editTaskForm = this.buildEditTaskForm();
+  }
+
+  onEditTaskClick() {
+
+  }
+
+  private buildEditTaskForm(): FormGroup {
+    return this.fb.group(
+      {
+        title: [null, Validators.required],
+        description: [null, Validators.required],
+        assignedTo: [null, Validators.required]
+      }
+    );
+
+  }
+
+  private getDeveloperData() {
+    this.isLoading = true;
+    this.projectId = this.active.snapshot.paramMap.get('id');
+    this.projectDataService.getprojectDetails(this.projectId).subscribe((project: Project<User>) => {
+      this.developers = project.developers;
+      this.projectName = project.name;
+      this.isLoading = false;
+    }, (error) => {
+      console.log('error', error);
+    });
+
   }
 
 }
